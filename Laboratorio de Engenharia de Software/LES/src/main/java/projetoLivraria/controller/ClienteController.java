832bb493746cb2bdcea9cd9410c15c8bbc39d1c5
Rita Muniz <ritamuniz1995@gmail.com>
2026-02-22 17:53:58 -0300

    Adicionando telas novas

--- Laboratorio de Engenharia de Software/LES/src/main/java/projetoLivraria/controller/ClienteController.java
+++ Laboratorio de Engenharia de Software/LES/src/main/java/projetoLivraria/controller/ClienteController.java
@@ -16,21 +16,6 @@ public class ClienteController extends HttpServlet {
     protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String action = req.getParameter("action");
 
-        try{
-            if (action == null || action.equals("list")) {
-                req.getRequestDispatcher("/cliente_list.jsp").forward(req, resp);
-                return;
-            }
-
-            if (action.equals("form")) {
-                req.getRequestDispatcher("/cliente_form.jsp").forward(req, resp);
-                return;
-            }
-
-        } catch (Exception e) {
-            req.setAttribute("erro", e.getMessage());
-            req.getRequestDispatcher("/cliente_list.jsp").forward(req,resp);
-        }
     }
 
     protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
