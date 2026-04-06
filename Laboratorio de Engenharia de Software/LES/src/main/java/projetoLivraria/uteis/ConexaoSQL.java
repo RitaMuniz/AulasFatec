caed68ab2087613c4f6288f0f05aba83e5bdce52
Rita Muniz <ritamuniz1995@gmail.com>
2026-04-06 16:21:45 -0300

    caminho do caique

--- Laboratorio de Engenharia de Software/LES/src/main/java/projetoLivraria/uteis/ConexaoSQL.java
+++ Laboratorio de Engenharia de Software/LES/src/main/java/projetoLivraria/uteis/ConexaoSQL.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 public class ConexaoSQL {
 
     private static final String URL = "jdbc:sqlite:/home/usuario/Área de trabalho/GitHub/AulasFatec/Laboratorio de Engenharia de Software/LES/livraria.db";
+//    private static final String URL = "jdbc:sqlite:C:/Users/Caique FATEC/Documents/Trabalho LES Livraria/AulasFatec/Laboratorio de Engenharia de Software/LES/livraria.db";
 
     private static ConexaoSQL instance;
 
