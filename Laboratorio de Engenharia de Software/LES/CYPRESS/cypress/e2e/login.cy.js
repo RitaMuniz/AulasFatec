d48e726a80e26d9bb4557127bc02cd8e0c35019a
Caique Antunes <caique_antunes@outlook.com>
2026-04-20 22:10:48 -0300

    adicionado teste automatizado de login errado

--- Laboratorio de Engenharia de Software/LES/CYPRESS/cypress/e2e/login.cy.js
+++ Laboratorio de Engenharia de Software/LES/CYPRESS/cypress/e2e/login.cy.js
@@ -1,5 +1,49 @@
 describe('Login', () => {
 
+    // Cliente preenche email e senha errado
+    it('Realizar login com credenciais invalidas', () => {
+        // arrange
+        cy.visit('http://localhost:8080/LES/view/login.jsp')
+
+        // act
+        cy.get('[data-test="email"]').type('caiqueemailerrado@gmail.com')
+        cy.get('[data-test="senha"]').type('Caique123senhaerrada.')
+        cy.get('[data-test="entrar"]').click()
+
+        // assert
+        cy.get('[data-test="erro_login_errado"]').should('contain.text', 'E-mail ou senha inválidos.')
+        cy.url().should('eq', 'http://localhost:8080/LES/view/login.jsp?erro=1')
+    })
+
+    // Cliente preenche senha errado
+    it('Realizar login com senha errada', () => {
+        // arrange
+        cy.visit('http://localhost:8080/LES/view/login.jsp')
+
+        // act
+        cy.get('[data-test="email"]').type('ritamuniz1995@gmail.com')
+        cy.get('[data-test="senha"]').type('SenhaErrada.')
+        cy.get('[data-test="entrar"]').click()
+
+        // assert
+        cy.get('[data-test="erro_login_errado"]').should('contain.text', 'E-mail ou senha inválidos.')
+        cy.url().should('eq', 'http://localhost:8080/LES/view/login.jsp?erro=1')
+    })
+
+    // Cliente preenche email inexistente com senha no banco
+    it('Realizar login com email inexistente e senha no banco', () => {
+        // arrange
+        cy.visit('http://localhost:8080/LES/view/login.jsp')
+
+        // act
+        cy.get('[data-test="email"]').type('ritamunizemailerrado@gmail.com')
+        cy.get('[data-test="senha"]').type('Rita123.')
+        cy.get('[data-test="entrar"]').click()
+
+        // assert
+        cy.get('[data-test="erro_login_errado"]').should('contain.text', 'E-mail ou senha inválidos.')
+        cy.url().should('eq', 'http://localhost:8080/LES/view/login.jsp?erro=1')
+    })
 
     // cliente esta inativo
     it('Realizar login com cliente inativo', () => {
