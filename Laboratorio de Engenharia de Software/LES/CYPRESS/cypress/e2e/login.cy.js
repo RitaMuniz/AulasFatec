0919e780918f4f3c9d3caa082a5772fffe725c9a
Caique Antunes <caique_antunes@outlook.com>
2026-04-20 22:07:01 -0300

    adicionado teste automatizado de login inativo

--- Laboratorio de Engenharia de Software/LES/CYPRESS/cypress/e2e/login.cy.js
+++ Laboratorio de Engenharia de Software/LES/CYPRESS/cypress/e2e/login.cy.js
@@ -1,6 +1,21 @@
 describe('Login', () => {
 
 
+    // cliente esta inativo
+    it('Realizar login com cliente inativo', () => {
+        // arrange
+        cy.visit('http://localhost:8080/LES/view/login.jsp')
+
+        // act
+        cy.get('[data-test="email"]').type('odeteroitman@gmail.com')
+        cy.get('[data-test="senha"]').type('odete123')
+        cy.get('[data-test="entrar"]').click()
+
+        // assert
+        cy.get('[data-test="erro_cliente_inativo"]').should('contain.text', 'Sua conta está inativa. Entre em contato com o suporte.')
+        cy.url().should('eq', 'http://localhost:8080/LES/view/login.jsp?erro=2')
+    })
+
     // cliente logado com sucesso
     it('Realizar login com sucesso', () => {
         // arrange
