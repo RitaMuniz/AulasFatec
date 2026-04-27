708c8430f59f7607cf0b4cb8cf27a3ba4bdacfe3
Caique Antunes <caique_antunes@outlook.com>
2026-04-27 00:27:33 -0300

    Ajusta configuracao Cypress

--- Laboratorio de Engenharia de Software/LES/CYPRESS/cypress.config.js
+++ Laboratorio de Engenharia de Software/LES/CYPRESS/cypress.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = defineConfig({
     chromeWebSecurity: false, // Problemas de Cross origin
@@ -6,6 +8,19 @@ module.exports = defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
       // implement node event listeners here
+            on("task", {
+                resetDb() {
+                    const bancoBase = path.resolve(__dirname, "../livraria-base.db");
+                    const bancoTeste = path.resolve(__dirname, "../livraria.db");
+
+                    fs.copyFileSync(bancoBase, bancoTeste);
+
+                    return null;
+                }
+            });
+
+            return config;
+        }
     },
-  },
+
 });
